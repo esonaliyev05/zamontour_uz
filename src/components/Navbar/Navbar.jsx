2d385816687923afe14fa7cf3f68fd815ac67e74
Alyorbekjonxon <alyorbek391@gmail.com>
2025-02-20 14:35:50 +0500

    yangilanish

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import './Navbar.scss';
-import { FaTelegram } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-import { HiBars3BottomLeft } from 'react-icons/hi2';
-import { HiOutlineX } from 'react-icons/hi';
-import { useTranslation } from 'react-i18next'; // useTranslation hook'ini import qilish
+import React, { useEffect, useState } from "react";
+import "./Navbar.scss";
+import { FaTelegram } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import { HiOutlineX } from "react-icons/hi";
+import { useTranslation } from "react-i18next"; // useTranslation hook'ini import qilish
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
@@ -16,10 +16,10 @@ const Navbar = () => {
 
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang).then(() => {
-      // window.location.reload(); 
+      // window.location.reload();
     });
   };
-  
+
   const [isHidden, setIsHidden] = useState(false);
   const [lastScrollY, setLastScrollY] = useState(0);
 
@@ -47,34 +47,55 @@ const Navbar = () => {
     };
   }, [lastScrollY]);
 
-  
-
   return (
     <nav className={`navbar ${isHidden ? "hidden" : ""}`}>
       <div className="container">
         <div className="logo">
-          <img src="/Navbar/zamon.logo.svg"  loading="lazy" alt="Description" />
+          <a href="#">
+            <h1>Aventour</h1>
+          </a>
         </div>
 
-        <ul className={open ? 'nav-links active' : 'nav-links'}>
-          <li ><a href="#">{t('Bosh sahifa')}</a></li>
-          <li><a href="#about">{t('Biz haqimizda')}</a></li>
-          <li><a href="#tours">{t('Turlar')}</a></li>
-          <li><a href="#concat">{t('Kontaktlar')}</a></li>
+        <ul className={open ? "nav-links active" : "nav-links"}>
+          <li>
+            <a href="#">{t("Bosh sahifa")}</a>
+          </li>
+          <li>
+            <a href="#about">{t("Biz haqimizda")}</a>
+          </li>
+          <li>
+            <a href="#tours">{t("Turlar")}</a>
+          </li>
+          <li>
+            <a href="#concat">{t("Kontaktlar")}</a>
+          </li>
         </ul>
 
         <div className="nav-concat">
           <div className="nav-languech">
-            <div className="translate" onClick={() => changeLanguage('uz')}>Uz</div>
-            <div className="translate" onClick={() => changeLanguage('en')}>En</div>
-            <div className="translate" onClick={() => changeLanguage('ru')}>Ru</div>
+            <div className="translate" onClick={() => changeLanguage("uz")}>
+              Uz
+            </div>
+            <div className="translate" onClick={() => changeLanguage("en")}>
+              En
+            </div>
+            <div className="translate" onClick={() => changeLanguage("ru")}>
+              Ru
+            </div>
           </div>
 
           <div className="icons">
             <div className="icon">
-              <FaTelegram onClick={() => handleRedirect("https://t.me/zamonbiznestour")} />
+              <FaTelegram
+                onClick={() => handleRedirect("https://t.me/zamonbiznestour")}
+              />
             </div>
-            <div className="icon" onClick={() => handleRedirect("https://www.instagram.com/zamontour/#")}>
+            <div
+              className="icon"
+              onClick={() =>
+                handleRedirect("https://www.instagram.com/zamontour/#")
+              }
+            >
               <FaInstagram />
             </div>
 
@@ -85,23 +106,45 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className={open ? 'Open activ' : 'Open'}>
+      <div className={open ? "Open activ" : "Open"}>
         <ul>
-        <li onClick={() => setOpen(false)}><a href="#">{t('Bosh sahifa')}</a></li>
-          <li onClick={() => setOpen(false)}><a href="#about">{t('Biz haqimizda')}</a></li>
-          <li onClick={() => setOpen(false)}><a href="#tours">{t('Turlar')}</a></li>
-          <li onClick={() => setOpen(false)}><a href="#concat">{t('Kontaktlar')}</a></li>
+          <li onClick={() => setOpen(false)}>
+            <a href="#">{t("Bosh sahifa")}</a>
+          </li>
+          <li onClick={() => setOpen(false)}>
+            <a href="#about">{t("Biz haqimizda")}</a>
+          </li>
+          <li onClick={() => setOpen(false)}>
+            <a href="#tours">{t("Turlar")}</a>
+          </li>
+          <li onClick={() => setOpen(false)}>
+            <a href="#concat">{t("Kontaktlar")}</a>
+          </li>
         </ul>
         <div className="nav-languech">
-          <div className="translate" onClick={() => changeLanguage('uz')}>Uz</div>
-          <div className="translate" onClick={() => changeLanguage('en')}>En</div>
-          <div className="translate" onClick={() => changeLanguage('ru')}>Ru</div>
+          <div className="translate" onClick={() => changeLanguage("uz")}>
+            Uz
+          </div>
+          <div className="translate" onClick={() => changeLanguage("en")}>
+            En
+          </div>
+          <div className="translate" onClick={() => changeLanguage("ru")}>
+            Ru
+          </div>
         </div>
         <div className="icons">
-          <div className="icon" onClick={() => handleRedirect("https://t.me/zamonbiznestour")}>
+          <div
+            className="icon"
+            onClick={() => handleRedirect("https://t.me/zamonbiznestour")}
+          >
             <FaTelegram />
           </div>
-          <div className="icon" onClick={() => handleRedirect("https://www.instagram.com/zamontour/#")}>
+          <div
+            className="icon"
+            onClick={() =>
+              handleRedirect("https://www.instagram.com/zamontour/#")
+            }
+          >
             <FaInstagram />
           </div>
         </div>
